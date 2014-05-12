dd6a2bfef4fae732cd8e593e0b7cae0a76f1f1f5
Niti Verma <contactnitiverma@gmail.com>
2014-05-11 18:33:50 -0700

    Add all routes for boards.

---
+++ src/README.md
@@ -0,0 +1,7 @@
+README
+
+Assumptions
+-----------
+
+# Create board assumes that the isLogin function is already present which will verify if the user is logged in or not. Get_logged_in function will actually check the value of the cookie to determine if the person is logged in.
+# Board with the same name can be created for now. We have not added a check to see if the board with the same name already exists.
--- src/getboards.py
+++ src/getboards.py
@@ -1,6 +1,7 @@
 # Author: Niti Verma
 from bottle import get, post, request, run, response, Bottle # or route
 import bottle
+from uuid import uuid4
 from bottle.ext import couchdb
 
 app = Bottle()
@@ -9,16 +10,84 @@ app = Bottle()
 plugin = couchdb.Plugin('pinterest_db', server_name='http://localhost:5984')
 app.install(plugin)
 
+def is_logged_in():
+  return True
+
+@app.route('/v1/user/:user_id/board/', method='POST')
+def create_board(user_id, db):
+  try:
+    if not is_logged_in():
+      abort(401, 'You have to login to create a board.')
+    doc_id = 'board:' + uuid4().hex
+    db[doc_id] = {'name': request.json['boardname'], 'owner': user_id}
+    response.status = 201
+    return 'Token: '+ doc_id
+  except couchdb.ResourceNotFound:
+    return bottle.HTTPError(404, "Page not found")
+
+@app.route('/v1/user/:user_id/board/:board_id/', method='DELETE')
+def delete_board(user_id, board_id, db):
+  try:
+    if not is_logged_in():
+      abort(401, 'You have to login to delete a board.')
+    board = db[board_id]
+    if user_id!= board['owner']:
+      abort(401, 'You do not have right to delete this board.')
+    db.delete(board)
+    response.status = 201
+    return 'Board deleted.'
+  except couchdb.ResourceNotFound:
+    return bottle.HTTPError(404, "Page not found")
+
+
+def getMapFun(field):
+  return '''function(doc) {
+      if (doc._id.indexOf('%s:') === 0) {
+        emit(doc._id, doc.name);
+      }
+    }''' % (field)
+
+@app.route('/v1/boards/:board_id')
+def get_board(board_id, db):
+  try:
+    doc = db[board_id]
+    pin_ids = doc['pins']
+    map_fun = '''function(doc) {
+      if (doc._id.indexOf('pins:') === 0) {
+        emit(doc._id, doc);
+      }
+    }'''
+    pins = []
+    for row in db.query(map_fun):
+      if row.key in pin_ids:
+        pins.append({'pin_name': row.value['pin_name'], 'pin_url': row.value['pin_url']})
+    return {'pins': pins}
+  except couchdb.ResourceNotFound:
+    return bottle.HTTPError(404, "Page not found")
+
 @app.route('/v1/boards')
 def get_boards(db):
   try:
-    map_fun = '''function(doc) {
-      emit(doc._id, doc.name);
-    }'''
+    map_fun = getMapFun('board')
     boards = []
     for row in db.query(map_fun):
       boards.append({'board_id': row.key, 'board_name': row.value})
     return {'boards': boards}
+  except couchdb.ResourceNotFound:
+    return bottle.HTTPError(404, "Page not found")
+
+@app.route('/v1/user/:user_id')
+def get_user_boards(user_id, db):
+  try:
+    if not is_logged_in():
+      abort(401, 'You have to login to view your boards.')
+    user = db[user_id]
+    map_fun = getMapFun('board')
+    boards = []
+    for row in db.query(map_fun):
+      if row.key in user['boards']:
+        boards.append({'board_id': row.key, 'board_name': row.value})
+    return {'name': user['name'], 'boards': boards}
   except couchdb.ResourceNotFound:
     return bottle.HTTPError(404, "Page not found")
 
